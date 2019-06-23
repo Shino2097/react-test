362cd67bdd8dc8217ed40dac7f01135955b5bc33
Shuma Terasawa <shino2097@gmail.com>
2019-06-24 04:18:38 +0900

    use React Fragment and use const and use script

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
+    const greeting = "Hi Tom";
+    const dom = <h1 className="foo">{greeting}</h1>;
     return (
-      <h1>
-        Hello, World!
-      </h1>
-    )
+      <React.Fragment>
+        {dom}
+        <input type="text" onChange={() => {console.log("clicked")}} />
+      </React.Fragment>
+    );
   }
 }
 
