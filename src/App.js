3a6022f87e7b46cafe0f398438b63cd26287152b
Shuma Terasawa <shino2097@gmail.com>
2019-06-24 03:42:48 +0900

    change render Component and h1 tag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div >
-        <h1>
-          Hello, World!!!
-        </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <h1>
+        Hello, World!
+      </h1>
+    )
+  }
 }
 
 export default App;
