1eea5be87b18f175a95cc5feb9d8ee4bb39ebc43
Shuma Terasawa <shino2097@gmail.com>
2019-06-26 00:50:28 +0900

    クラスコンポーネントと関数コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = "Hi Tom";
-    const dom = <h1 className="foo">{greeting}</h1>;
-    return (
-      <React.Fragment>
-        {dom}
-        <input type="text" onChange={() => {console.log("clicked")}} />
-      </React.Fragment>
-    );
-  }
+//クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi Tom";
+//     const dom = <h1 className="foo">{greeting}</h1>;
+//     return (
+//       <React.Fragment>
+//         {dom}
+//         <input type="text" onChange={() => {console.log("clicked")}} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+//関数コンポーネント
+const App = () => {
+  return (
+    <div>
+      <Cat ></Cat>
+      <Cat></Cat>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>meow</div>
 }
 
 export default App;
