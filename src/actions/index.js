29f4c15e0c54c95eafba152a574551a046549458
Shuma Terasawa <shino2097@gmail.com>
2019-06-29 17:22:27 +0900

    Appを変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 //あとでimportできるように、変数化しておく
-export const INCREMENT = "INCREMENT"
-export const DECREMENT = "DECREMENT"
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
-export const INCREMENT = () => ({
+export const increment = () => ({
   type: INCREMENT
 });
 
 //書き方はどっちでもいいかも
-export const DECREMENT = () => {
+export const decrement = () => {
   return {
     type: DECREMENT
   };
